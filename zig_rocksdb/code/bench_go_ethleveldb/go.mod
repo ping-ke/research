0d29adca521c1059f8e48bc4a96e92b676375b1a
ping-ke <molaokp@gmail.com>
2025-10-24 10:51:04 +0800

    add bench_go_ethleveldb

---
+++ zig_rocksdb/code/bench_go_ethleveldb/go.mod
@@ -0,0 +1,20 @@
+module bench_go_leveldb
+
+go 1.24.9
+
+require (
+	github.com/ethereum/go-ethereum v1.16.5
+	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
+)
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+)
---
+++ zig_rocksdb/code/bench_go_ethleveldb/main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/ethereum/go-ethereum/ethdb/leveldb"
+	"github.com/ethereum/go-ethereum/log"
+)
+
+const (
+	valLen = 110
+	keyLen = 32
+)
+
+var (
+	ni       = flag.Bool("needInit", false, "Need to insert kvs before test, default false")
+	tc       = flag.Int64("total", 4000000000, "Number of kvs to insert before test, default value is 4_000_000_000")
+	wc       = flag.Int64("write", 10000000, "Number of write count during the test")
+	rc       = flag.Int64("read", 10000000, "Number of read count during the test")
+	t        = flag.Int64("threads", 32, "Number of threads")
+	bi       = flag.Bool("batchInsert", true, "Enable batch insert or not")
+	dbPath   = flag.String("dbpath", "./data/bench_go_leveldb", "Data directory for the databases")
+	logLevel = flag.Int64("loglevel", 3, "Log level")
+)
+
+var (
+	randBytes = make([]byte, valLen*keyLen)
+)
+
+func randomWrite(tid, count, start, end int64, db *leveldb.Database, wg *sync.WaitGroup) {
+	defer wg.Done()
+	st := time.Now()
+	key := make([]byte, keyLen)
+	r := rand.New(rand.NewSource(time.Now().UnixNano() + tid))
+	for i := int64(0); i < count; i++ {
+		rv := r.Int63n(end-start) + start
+		s := (rv % keyLen) * valLen
+		binary.BigEndian.PutUint64(key[keyLen-8:keyLen], uint64(rv))
+		_ = db.Put(key, randBytes[s:s+valLen])
+		if *logLevel >= 3 && i%1000000 == 0 && i > 0 {
+			ms := time.Since(st).Milliseconds()
+			fmt.Printf("thread %d used time %d ms, hps %d\n", tid, ms, i*1000/ms)
+		}
+	}
+	if *logLevel >= 3 {
+		tu := time.Since(st).Seconds()
+		fmt.Printf("thread %d random write done %.2fs, %.2f ops/s\n", tid, tu, float64(count)/tu)
+	}
+}
+
+func randomRead(tid, count, start, end int64, db *leveldb.Database, wg *sync.WaitGroup) {
+	defer wg.Done()
+	st := time.Now()
+	key := make([]byte, keyLen)
+	r := rand.New(rand.NewSource(time.Now().UnixNano() + tid))
+
+	for i := int64(0); i < count; i++ {
+		rv := r.Int63n(end-start) + start
+		binary.BigEndian.PutUint64(key[keyLen-8:keyLen], uint64(rv))
+		_, _ = db.Get(key)
+		if *logLevel >= 3 && i%1000000 == 0 && i > 0 {
+			ms := time.Since(st).Milliseconds()
+			fmt.Printf("thread %d used time %d ms, hps %d\n", tid, ms, i*1000/ms)
+		}
+	}
+	if *logLevel >= 3 {
+		tu := time.Since(st).Seconds()
+		fmt.Printf("thread %d random read done %.2fs, %.2f ops/s\n", tid, tu, float64(count)/tu)
+	}
+}
+
+func seqWrite(tid, count int64, db *leveldb.Database, wg *sync.WaitGroup) {
+	defer wg.Done()
+	st := time.Now()
+	key := make([]byte, keyLen)
+	for i := int64(0); i < count; i++ {
+		idx := tid*count + i
+		s := (idx % keyLen) * valLen
+		binary.BigEndian.PutUint64(key[keyLen-8:keyLen], uint64(idx))
+		_ = db.Put(key, randBytes[s:s+valLen])
+		if *logLevel >= 3 && i%1000000 == 0 && i > 0 {
+			ms := time.Since(st).Milliseconds()
+			fmt.Printf("thread %d used time %d ms, hps %d\n", tid, ms, i*1000/ms)
+		}
+	}
+	if *logLevel >= 3 {
+		tu := time.Since(st).Seconds()
+		fmt.Printf("thread %d seq write done %.2fs, %.2f ops/s\n", tid, tu, float64(count)/tu)
+	}
+}
+
+func seqRead(tid, count int64, db *leveldb.Database, wg *sync.WaitGroup) {
+	defer wg.Done()
+	st := time.Now()
+	key := make([]byte, keyLen)
+	for i := int64(0); i < count; i++ {
+		binary.BigEndian.PutUint64(key[keyLen-8:keyLen], uint64(tid*count+i))
+		_, _ = db.Get(key)
+		if *logLevel >= 3 && i%1000000 == 0 && i > 0 {
+			ms := time.Since(st).Microseconds()
+			fmt.Printf("thread %d used time %d ms, hps %d\n", tid, ms, i*1000/ms)
+		}
+	}
+	if *logLevel >= 3 {
+		tu := time.Since(st).Seconds()
+		fmt.Printf("thread %d seq read done %.2fs, %.2f ops/s\n", tid, tu, float64(count)/tu)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	db, err := leveldb.New(*dbPath, 2048, 8196, "./data/bench_go_leveldb", false)
+	if err != nil {
+		log.Crit("New dashboard fail", "err", err)
+	} else {
+		defer db.Close()
+	}
+
+	threads := *t
+	total := *tc
+	writeCount := *wc
+	readCount := *rc
+	fmt.Printf("Threads: %d\n", threads)
+	fmt.Printf("Total data: %d while needInit=%t\n", total, *ni)
+	fmt.Printf("Ops: %d write ops and %d read ops\n", writeCount, readCount)
+
+	rand.Read(randBytes)
+
+	var wg sync.WaitGroup
+	if *ni && total > 0 {
+		start := time.Now()
+		per := total / threads
+		for tid := int64(0); tid < threads; tid++ {
+			wg.Add(1)
+			id := tid
+			go seqWrite(id, per, db, &wg)
+		}
+		wg.Wait()
+		ms := float64(time.Since(start).Milliseconds())
+		fmt.Printf("Init write: %d ops in %.2f ms (%.2f ops/s)\n", total, ms, float64(total)*1000/ms)
+	}
+
+	if writeCount > 0 {
+		start := time.Now()
+		per := writeCount / threads
+		for tid := int64(0); tid < threads; tid++ {
+			wg.Add(1)
+			id := tid
+			go randomWrite(id, per, 0, total, db, &wg)
+		}
+		wg.Wait()
+		ms := float64(time.Since(start).Milliseconds())
+		fmt.Printf("Random update: %d ops in %.2f ms (%.2f ops/s)\n", writeCount, ms, float64(writeCount)*1000/ms)
+	}
+
+	if readCount > 0 {
+		start := time.Now()
+		per := readCount / threads
+		for tid := int64(0); tid < threads; tid++ {
+			wg.Add(1)
+			id := tid
+			go randomRead(id, per, 0, total, db, &wg)
+		}
+		wg.Wait()
+		ms := float64(time.Since(start).Milliseconds())
+		fmt.Printf("Random read: %d ops in %.2f ms (%.2f ops/s)\n", readCount, ms, float64(readCount)*1000/ms)
+	}
+}
